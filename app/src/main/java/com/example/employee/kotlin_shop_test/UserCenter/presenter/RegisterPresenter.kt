abe6850e1ed4c135681d8daccd1a9264973a3e07
clown <568317419@qq.com>
2019-09-20 00:55:07 +0800

    修改

--- app/src/main/java/com/example/employee/kotlin_shop_test/UserCenter/presenter/RegisterPresenter.kt
+++ app/src/main/java/com/example/employee/kotlin_shop_test/UserCenter/presenter/RegisterPresenter.kt
@@ -1,10 +1,11 @@
 package com.example.employee.kotlin_shop_test.UserCenter.presenter
 
+import android.util.Log
 import com.example.employee.kotlin_shop_test.BaseLibrary.presenter.BasePresenter
 import com.example.employee.kotlin_shop_test.UserCenter.presenter.view.RegisterView
 
 class RegisterPresenter :BasePresenter<RegisterView>() {
     private fun check(){
-
+            Log.e("TAg","")
     }
 }
